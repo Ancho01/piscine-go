82f74e629a648ef470d9a2dc79c016ae488f56c9
Ancho01 <anuarmaratuly01@gmail.com>
2019-10-15 22:37:27 +0600

    Zbs

--- myfamily.sh
+++ myfamily.sh
@@ -1,6 +1,6 @@
 
 curl -O -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json 
-cat all.json | jq ".[] | select(.id == $HERO_ID ) |  .connections  | .relatives"  
+cat all.json | jq ".[] | select(.id == $HERO_ID ) |  .connections  | .relatives" |sed 's/"//g'  
  
 
 
