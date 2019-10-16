63f6027f25cf1d8e9e90a15ba250e5ce1311f51a
Ancho01 <anuarmaratuly01@gmail.com>
2019-10-17 01:03:55 +0600

    kaka

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+ package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	next := false
+	for x := '0'; x <= '9'; x++ {
+		for y := x + 1; y <= '9'; y++ {
+			for z := y + 1; z <= '9'; z++ {
+				if next {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				next = true
+				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
