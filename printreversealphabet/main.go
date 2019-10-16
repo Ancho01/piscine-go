52a47708e9cb764707b86b81a5bf19948e8cb7d1
Ancho01 <anuarmaratuly01@gmail.com>
2019-10-16 17:14:40 +0600

    resalph

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,11 +4,11 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-    for i := 'z'; i >= 'a'; i-- {
-        z01.PrintRune(i)
-        if i == 'a' {
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(i)
+		if i == 'a' {
 
-            z01.PrintRune(10)
-        }
-    }
+			z01.PrintRune(10)
+		}
+	}
 }
